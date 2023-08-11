890a9d806de6043ae7b3e6ffc2d6d5d7d98822b0
CasandraSucala <casandra.sucala.@gmail.com>
2023-08-11 20:37:05 +0300

    Create a child component to demonstrate how @Input and @Output work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
+import {AppComponent} from './app.component';
+import {HelloComponent} from './hello/hello.component';
 import {FormsModule} from "@angular/forms";
+import {ChildComponent} from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
+    HelloComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
@@ -17,4 +19,5 @@ import {FormsModule} from "@angular/forms";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
